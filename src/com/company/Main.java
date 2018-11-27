a6579e98b39776d463424428fa8aa2c25e98c9d8
glivas <giorgos.livas.99@gmail.com>
2018-11-27 23:32:08 +0000

    remove douplicate array

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -24,12 +24,12 @@ public class Main {
         printChessBoard();
     }
 
-    public static void makeBoard(int y) {
+    private static void makeBoard(int y) {
         IntStream.range(0, y).boxed().forEach(i -> {List<Cell> t = new ArrayList<Cell>(); board.add(t);});
         IntStream.range(0, y).boxed().forEach(i -> board.stream().forEach(e -> e.add(new Cell(" "))));
     }
 
-    public static int[] readPositionFromConsole() {
+    private static int[] readPositionFromConsole() {
         Scanner sc = new Scanner(System.in);
         out.println("Enter a position on the board.  (i.e. E2)");
         String pos = sc.nextLine();
@@ -37,7 +37,7 @@ public class Main {
         return validatePosition(pos);
     }
 
-    public static int[] validatePosition(String position) {
+    private static int[] validatePosition(String position) {
         char[] pos = position.toCharArray();
 
         if (pos.length == 2) {
@@ -48,15 +48,14 @@ public class Main {
             int y = Integer.parseInt(ys);
 
             if (x > 0 && x <= 8 && y <= 8) {
-                int[] array = {x, y};
-                return array;
+                return new int[]{x, y};
             }
         }
-        int[] array = {0, 0};
-        return array;
+
+        return new int[]{0, 0};
     }
 
-    public static void addQueen(int[] pos, boolean byUser) {
+    private static void addQueen(int[] pos, boolean byUser) {
         int x = pos[0]-1;
         int y = pos[1]-1;
 
@@ -71,8 +70,8 @@ public class Main {
                                         || (i == y - k && j == x + k)
                                         || (i == y - k && j == x - k))
                                         && board.get(i).get(j).isAvailable()) {
-                                    board.get(i).get(j).setContent(".");
-                                    // board.get(i).get(j).setAvailable(false);
+                                    // board.get(i).get(j).setContent(".");
+                                    board.get(i).get(j).setAvailable(false);
                                 }
                             })
                     )
@@ -80,7 +79,7 @@ public class Main {
         }
     }
 
-    public static void fillWithQueens() {
+    private static void fillWithQueens() {
         IntStream.range(0, 8).boxed().forEach(i ->
                 IntStream.rangeClosed(1, 8).boxed().forEach(j ->
                         IntStream.rangeClosed(1, 8).boxed().forEach(y -> {
@@ -91,9 +90,9 @@ public class Main {
         );
     }
 
-    public static void printChessBoard() {
+    private static void printChessBoard() {
         out.print(" ");
-        Arrays.asList(positions).stream().forEach(a -> out.print("   " + a.toUpperCase()));
+        Arrays.asList(positions).forEach(a -> out.print("   " + a.toUpperCase()));
 
         out.print("\n  +");
 
@@ -101,11 +100,11 @@ public class Main {
 
         out.println();
 
-            board.stream()
+            board
                 .forEach(a -> {
                     out.print(board.indexOf(a)+1 + " ");
 
-                    a.stream().forEach(e -> out.print("| " + e + " "));
+                    a.forEach(e -> out.print("| " + e + " "));
 
                     out.print("|");
                     out.print("\n  +");
