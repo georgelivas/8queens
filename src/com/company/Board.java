acd2e999dec67d7079967da5bda088647767fa49
glivas <giorgos.livas.99@gmail.com>
2018-12-04 15:38:40 +0000

    create class board

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -1,4 +1,62 @@
 package com.company;
 
+import java.util.*;
+import java.util.stream.IntStream;
+
+import static java.lang.System.out;
+
 public class Board {
+    private List<String> positions = Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h");
+
+    private List<List<Cell>> board = new ArrayList<>();
+
+    public Board(int numOfCells) {
+        IntStream.range(0, numOfCells).boxed().forEach(i -> {List<Cell> t = new ArrayList<>(); board.add(t);});
+        IntStream.range(0, numOfCells).boxed().forEach(i -> board.forEach(e -> e.add(new Cell(" "))));
+    }
+
+    private void addQueen(int[] pos, boolean byUser) {
+        int x = pos[0]-1;
+        int y = pos[1]-1;
+
+        if (pos[0] != 0 && board.get(y).get(x).isAvailable()) {
+            board.get(y).get(x).setContent(byUser ? "♛" : "♕");
+            IntStream.range(0, 8).boxed().forEach(i ->
+                    IntStream.range(0, 8).boxed().forEach(j ->
+                            IntStream.range(0, 8).boxed().forEach(k -> {
+                                if (((i == y || j == x)
+                                        || (i == y + k && j == x + k)
+                                        || (i == y + k && j == x - k)
+                                        || (i == y - k && j == x + k)
+                                        || (i == y - k && j == x - k))
+                                        && board.get(i).get(j).isAvailable()) {
+                                    // board.get(i).get(j).setContent(".");
+                                    board.get(i).get(j).setAvailable(false);
+                                }
+                            })
+                    )
+            );
+        }
+    }
+
+    private void printChessBoard() {
+        out.print(" ");
+
+        positions.forEach(a -> out.print("   " + a.toUpperCase()));
+        out.print("\n  +");
+
+        IntStream.range(0, board.size()).boxed().forEach(e -> out.print("---+"));
+        out.println();
+
+        board.forEach(a -> {
+            out.print(board.indexOf(a)+1 + " ");
+
+            a.forEach(e -> out.print("| " + e + " "));
+            out.print("|\n  +");
+
+            IntStream.range(0, board.size()).boxed().forEach(e -> out.print("---+"));
+            out.println();
+        });
+        out.println("\n");
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -105,30 +105,6 @@ public class Main {
         return new int[]{0, 0};
     }
 
-    private static void addQueen(int[] pos, boolean byUser) {
-        int x = pos[0]-1;
-        int y = pos[1]-1;
-
-        if (pos[0] != 0 && board.get(y).get(x).isAvailable()) {
-            board.get(y).get(x).setContent(byUser ? "♛" : "♕");
-            IntStream.range(0, 8).boxed().forEach(i ->
-                    IntStream.range(0, 8).boxed().forEach(j ->
-                            IntStream.range(0, 8).boxed().forEach(k -> {
-                                if (((i == y || j == x)
-                                        || (i == y + k && j == x + k)
-                                        || (i == y + k && j == x - k)
-                                        || (i == y - k && j == x + k)
-                                        || (i == y - k && j == x - k))
-                                        && board.get(i).get(j).isAvailable()) {
-                                    // board.get(i).get(j).setContent(".");
-                                    board.get(i).get(j).setAvailable(false);
-                                }
-                            })
-                    )
-            );
-        }
-    }
-
     private static void fillWithQueens() {
         IntStream.range(0, 8).boxed().forEach(i ->
                 IntStream.rangeClosed(1, 8).boxed().forEach(j ->
@@ -137,26 +113,5 @@ public class Main {
                         )
                 )
         );
-    }
-
-    private static void printChessBoard() {
-        out.print(" ");
-
-        positions.forEach(a -> out.print("   " + a.toUpperCase()));
-        out.print("\n  +");
-
-        IntStream.range(0, board.size()).boxed().forEach(e -> out.print("---+"));
-        out.println();
-
-        board.forEach(a -> {
-                out.print(board.indexOf(a)+1 + " ");
-
-                a.forEach(e -> out.print("| " + e + " "));
-                out.print("|\n  +");
-
-                IntStream.range(0, board.size()).boxed().forEach(e -> out.print("---+"));
-                out.println();
-        });
-        out.println("\n");
     }
 }
